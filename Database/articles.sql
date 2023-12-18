8cc8cae7581551e4b9629c900fbd5a417dfc39a0
Bùi Thúy Ngọc <90141097+ngoc2003@users.noreply.github.com>
2023-12-18 18:13:15 +0700

    Feat/modify (#2)

--- Database/articles.sql
+++ Database/articles.sql
@@ -1,24 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 16, 2023 lúc 11:06 AM
--- Phiên bản máy phục vụ: 10.4.28-MariaDB
--- Phiên bản PHP: 8.2.4
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `article`
+-- Cơ sở dữ liệu: `myArticle`
 --
 
 -- --------------------------------------------------------
@@ -28,7 +10,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `articles` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL primary key auto_increment,
   `title` text NOT NULL,
   `content` varchar(100) CHARACTER SET armscii8 COLLATE armscii8_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -37,31 +19,6 @@ CREATE TABLE `articles` (
 -- Đang đổ dữ liệu cho bảng `articles`
 --
 
-INSERT INTO `articles` (`id`, `title`, `content`) VALUES
-(3, 'PHP', 'hoc de qua mon PHP'),
-(4, 'JAVA', 'hoc de qua mon JAVA');
-
---
--- Chỉ mục cho các bảng đã đổ
---
-
---
--- Chỉ mục cho bảng `articles`
---
-ALTER TABLE `articles`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT cho các bảng đã đổ
---
-
---
--- AUTO_INCREMENT cho bảng `articles`
---
-ALTER TABLE `articles`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `articles` (`title`, `content`) VALUES
+('PHP', 'hoc de qua mon PHP'),
+('JAVA', 'hoc de qua mon JAVA');
---
+++ README.md
@@ -0,0 +1,13 @@
+# BTTH03.1 - 63KTPM02
+
+---
+
+## Các thành viên trong nhóm:
+1. Chu Thị Ngọc Bích:
+
+2. Bùi THúy Ngọc:
+
+3. Trần THị Hiền Tiên:
+
+4. Nguyễn Thị Nga: 
+
